ae461983f82ef00136bbee5a966a299cad480003
estebanrm1 <solsel90@gmail.com>
2023-06-08 01:38:52 -0300

    Se termino el maquetado y la funcionalidad entre componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import BuscadorNoticias from './components/BuscadorNoticias'
+
 
 function App() {
   
 
+
   return (
-    <>
-      
-    </>
+    <section className='body1 container mt-3 text-center'>
+    <h1 className='p-3 text-bg-secondary rounded'>Todo Noticias</h1>
+    <hr />
+    <div>
+      <BuscadorNoticias></BuscadorNoticias>
+    </div>
+    </section>
   )
 }
 
--- src/components/BuscadorNoticias.jsx
+++ src/components/BuscadorNoticias.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
+import ListaNoticias from './ListaNoticias';
 
 const BuscadorNoticias = () => {
+
+    const [categoria, setCategoria] = useState("");
+    const [noticias, setNoticias] = useState([]);
+
+    useEffect(() => { consultarAPI(); }, [categoria]);
+
+    const consultarAPI = async ()=>{
+        try {
+            const respuesta = await fetch(`https://newsdata.io/api/1/news?apikey=pub_241309c01f186acaab8703dc7f674dc770f52&category=${categoria || "top"}&language=es`);
+            const resultadoNoticias = await respuesta.json();
+            setNoticias(resultadoNoticias.results);
+                console.log(consultarAPI)
+            } catch (error) {
+                console.log(error);
+            }
+    };
+
     return (
-        <div>
-            
-        </div>
+        <>
+        <Row className='justify-content-md-center'>
+            <Col md="auto">
+                <h6>Buscar noticias por categoria:</h6>
+                <Form.Select className='bg-transparent border-black' categoria= {categoria}  onChange={(e) => {
+                    setCategoria(e.target.value);
+                }}
+                value={categoria}
+                >
+            <option value="">Seleccionar categoria</option>
+            <option value="top">Mas vistas</option>
+            <option value="business">Negocios</option>
+            <option value="food">Gastronomia</option>
+            <option value="entertainment">Entretenimiento</option>
+            <option value="environment">Medio Ambiente</option>
+            <option value="health">Salud</option>
+            <option value="politics">Política</option>
+            <option value="sports">Deportes</option>
+            <option value="science">Ciencia</option>
+            <option value="technology">Tecnología</option>
+            <option value="tourism">Turismo</option>
+            <option value="world">El Mundo</option>
+                </Form.Select>
+            </Col>
+        </Row>
+        <hr />
+        <ListaNoticias noticias={noticias}></ListaNoticias>
+                </>
+
     );
 };
 
